c762a59ea8ba8781e9833bf923d74922554e6ae4
Dorota Wróbel-Mirowska <wrobel.d.h@gmail.com>
2022-10-15 13:45:03 +0200

    Create Speciality.java

---
+++ Speciality.java
@@ -0,0 +1,13 @@
+public class Speciality {
+  String name;
+  Course[] courses;
+  
+  class Course {
+    int semester;
+    String Name;
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+}
