d32fda8f3e4040879d1e2202eeca1f4560b3b3a0
Dorota Wrobel-Mirowska <dwrobel-mirowska@egnyte.com>
2022-11-05 14:55:40 +0100

    Add second task

---
+++ lesson-2/ex-2.java
@@ -0,0 +1,55 @@
+interface Animal {
+    public Animal giveFavouriteToy(String toyName);
+//    public int foodWeight();
+}
+
+public class Dog implements Animal {
+    private String name;
+    private String favouriteToy;
+
+    Dog(String name, String favouriteToy) {
+        this.name = name;
+        this.favouriteToy = favouriteToy;
+    }
+
+    @Override
+    public Animal giveFavouriteToy(String toyName) {
+        return new Dog(this.name, toyName);
+    }
+
+    public String toString() {
+        return String.format("Dog %s plays with %s", this.name, this.favouriteToy);
+    }
+}
+
+public class Cat implements Animal {
+    private String name;
+    private String favouriteToy;
+
+    Cat(String name, String favouriteToy) {
+        this.name = name;
+        this.favouriteToy = favouriteToy;
+    }
+
+    @Override
+    public Animal giveFavouriteToy(String toyName) {
+        return new Cat(this.name, toyName);
+    }
+
+    public String toString() {
+        return String.format("Cat %s plays with %s", this.name, this.favouriteToy);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal burekWithBone = new Dog("Burek", "Bone");
+        Animal burekWithBall = burekWithBone.giveFavouriteToy("Ball");
+        Cat rudyCat = new Cat("Rudy", "Mouse");
+        rudyCat.giveFavouriteToy("Feather Wand");
+
+        System.out.println(burekWithBone);
+        System.out.println(burekWithBall);
+    }
+}
