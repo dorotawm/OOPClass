104ed6fdf2714ca9de80c823f6bcae42a5bf5d76
Dorota Wrobel-Mirowska <dwrobel-mirowska@egnyte.com>
2022-12-10 14:31:44 +0100

    Print balances

--- lesson-3/task1/src/Main.java
+++ lesson-3/task1/src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Euro euro = new Euro(200);
         Euro euro2 = euro.subtractedCurrency(100, "GBP");
+        System.out.println(euro.balance());
         System.out.println(euro2.balance());
-        System.out.println(euro.symbol());
-
     }
 }
